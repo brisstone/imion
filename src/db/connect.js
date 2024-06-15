88b3c10280e708bb246b4f82ff879c550cd13569
Colin-nwk <collinsnwoko05@gmail.com>
2024-06-15 12:01:12 +0100

    minor fix

--- src/db/connect.js
+++ src/db/connect.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB_URI);
+    await mongoose.connect(`${process.env.MONGO_DB_URI}`);
     console.log("connected");
   } catch (error) {
     console.log("Error connecting to mongoDB", error.message);
